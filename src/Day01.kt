5410941a73e18e82ee41ce4949faa1677323be7f
basenech <christoph.basener@ista.com>
2021-12-01 13:48:32 +0100

    AoC day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,38 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val intArray = input.map { it.toInt() }.toIntArray()
+
+        var increased = 0
+
+        for (i in 1 until intArray.size) {
+            if (intArray[i] > intArray[i - 1])
+                increased++
+        }
+
+
+        return increased
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intArray = input.map { it.toInt() }.toIntArray()
+
+        var increased = 0
+
+        for (i in 0..intArray.size - 4) {
+            val firstSlidingWindow = intArray[i] + intArray[i + 1] + intArray[i + 2]
+            val secondSlidingWindow = intArray[i + 1] + intArray[i + 2] + intArray[i + 3]
+
+            if (secondSlidingWindow > firstSlidingWindow)
+                increased++
+        }
+
+        return increased
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
